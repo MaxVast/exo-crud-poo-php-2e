8f4bb4621a430464795091ee6ff273f0ae72ed05
Maxence Vast <34278027+MaxVast@users.noreply.github.com>
2024-10-10 12:00:15 +0200

    Update User.php

--- App/Entity/User.php
+++ App/Entity/User.php
@@ -8,12 +8,17 @@ class User {
     private $mail;
     private $password;
     private $media_object;
+    private $created_at;
+    private $last_connection;
 
-    public function __construct($username, $mail, $password, $media_object) {
+    public function __construct($username, $mail, $password, $media_object,  $id = null, $created_at = null, $last_connection = null) {
+        $this->id = $id;
         $this->username = $username;
         $this->mail = $mail;
         $this->password = $password;
         $this->media_object = $media_object;
+        $this->created_at = $created_at;
+        $this->last_connection = $last_connection;
     }
 
     public function getId() {
@@ -28,7 +33,19 @@ class User {
         return $this->mail;
     }
 
+    public function getPassword() : string {
+        return $this->password;
+    }
+
     public function getMediaObject() : string {
         return $this->media_object;
+    }
+
+    public function getCreatedAt() : ?string {
+        return $this->created_at;
+    }
+
+    public function getLastConnection() : ?string {
+        return $this->last_connection;
     }
 }
